b8861c132d66a61e47018c256e7b82680a7b715b
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-07-18 16:18:54 +0000

    Excuse Generator finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,49 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+/*  
+Using javascript, create a function that generates and returns a random excuse with the following structure:
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+To create a consistent excuse you have to concatenate one item from each array in the proper order.
+Call that function onLoad and set the excuse into the innerHTML of the #excuse HTML element.
+*/
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  console.log(excuseGen());
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excuseGen() {
+  //who
+  var subject = Math.floor(Math.random() * (who.length - 1) + 1);
+  //action
+  var verb = Math.floor(Math.random() * (action.length - 1) + 1);
+  //what
+  var complement = Math.floor(Math.random() * (what.length - 1) + 1);
+  //when
+  var time = Math.floor(Math.random() * (when.length - 1) + 1);
+
+  //concatenating the array and setting into the p
+  document.getElementById("excuse").innerHTML =
+    who[subject] +
+    " " +
+    action[verb] +
+    " " +
+    what[complement] +
+    " " +
+    when[time];
+}
